40026602689da739c025199e5629bb97af18415d
KY00KIM <hived@naver.com>
2023-09-01 14:55:54 +0900

    Add logger

---
+++ logger.py
@@ -0,0 +1,12 @@
+import logging 
+from config import LOG_PATH
+
+
+# @@ Logging ############################
+
+logging.basicConfig(level=logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler = logging.FileHandler(LOG_PATH, mode="a")
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
