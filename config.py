48405fc1182374f0da1f6925469b7d3b5d0a7955
KY00KIM <hived@naver.com>
2023-09-06 08:11:30 +0900

    Fix timeout

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ LOG_PATH = "scheduler.log"
 ENGINE_URLS = json.loads(os.environ['ENGINE_URLS'])
 VALID_STATE = [-1, 0, 1]
 ENGINE_STATUS_TIMEOUT = 20
-ENGINE_PROCESS_TIMEOUT = 500
+ENGINE_PROCESS_TIMEOUT = 1200
 WAS_API_BASE_URL = os.environ['WAS_API_BASE_URL']
 VALID_JOB_STATE_STR = ["pending", "in_process", "processed", "error"]
